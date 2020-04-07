94878764f4a59715b100e1088fd89b7189441636
Miguek Cardona <37484798+micardona96@users.noreply.github.com>
2020-04-06 23:52:41 -0500

    Update mixer.rkt

--- Mixer/mixer.rkt
+++ Mixer/mixer.rkt
@@ -3,140 +3,6 @@
 #|
     Created by Miguel Cardona on 04/04/20.
     Copyright © 2020 Miguel Cardona 1628209. All rights reserved.
-
-< programa >
-                :=  < expresion > 
-                un-programa (exp)
-
-< expresion >
-                :=  < numero >
-                número-exp (num)
-
-                := true  
-                true-exp
-
-                := false 
-                false-exp
-
-                :=  \'  < texto >  \' 
-                texto-exp (txt)
-
-                :=  < identificador >  
-                id-exp ( id)
-
-                :=  if  < expresion >  {  < expresion >  } else {  < expresion >  }  
-                condicional-exp (cond  true-exp  false-exp)
-
-                :=  private def ( < def-privada , * > ) {  < expresion >  }
-                definicion-exp (def-privadas body)
-
-                :=  x32  [  < numero * >  ]  
-                base-32-exp (nums)
-
-                :=  x16  [  < numero * >  ]  
-                base-16-exp (nums)
-
-                :=  x8  [  < numero * >  ]  
-                base-8-exp (nums)
-
-                := [  < expresion > , *  ] 
-                lista-exp (exps)
-
-                :=  (< expresion >  < primitiva-binaria >  < expresion > )  
-                op-binaria-exp (rand primitive-bin rator)
-
-                :=  < primitiva-unaria >  (< expresion > )  
-                op-unaria-exp (primitive-un exp)
-
-                :=  func (< identificador > , * ) = {  < expresion >  }  
-                función-exp (ids exp)
-
-                :=  import (< expresion >  (< expresion > * )  
-                ejecutar-function-exp (ids  exp)
-
-                :=  export func  < identificador >  (< identificador > , * ) ={  < expresion >  }  
-                función-rec-exp (id  ids  exp)
-
-
-< def-privada >
-
-                :=  const  < identificador >  =  < expresion > 
-                constante (id  exp)
-
-                :=  static  < identificador > 
-                crear-var (id)
-
-                :=  init static  < identificador >  =  < expresion > 
-                asignar-var (id  exp)
-
-
-< primitiva-unaria >
-
-        base-32
-                :=  ++32  [ sumar-uno-32 ]
-                :=  --32  [ restar-uno-32 ]
-        base-16
-                :=  ++16  [ sumar-uno-16 ]
-                :=  --16  [ restar-uno-16 ]
-
-         base-8
-                :=  ++8  [ sumar-uno-8 ]
-                :=  --8  [ restar-uno-8 ]
-
-       decimales
-                :=  !   [ negación ]
-                :=  ++  [ sumar-uno ]
-                :=  --  [ restar-uno ]
-
-        strings
-                :=  strlen  [ longitud ]
-
-         listas
-                :=  isNull  [ es-vacía? ]
-                :=  isList  [ es-lista? ]
-                :=  pop  [ primer-ítem ]
-                :=  next [ resto-items ]
-
-
-< primitiva-binaria >
-
-         base-32
-                :=  +32  [ sumar-32 ]
-                :=  -32  [ restar-32 ]
-                :=  *32  [ multiplicar-32 ]
-
-         base-16
-                :=  +32  [ sumar-16 ]
-                :=  -32  [ restar-16 ]
-                :=  *32  [ multiplicar-16 ]
-
-          base-8
-                :=  +8  [ sumar-8 ]
-                :=  -8  [ restar-8 ]
-                :=  *8  [ multiplicar-8 ]
-
-       decimales
-                :=  >  [ mayor ]
-                :=  >= [ mayor-o-igual]
-                :=  <  [ menor ]
-                :=  <= [ menor-o-igual ]
-                :=  == [ igual ]
-                :=  && [ and ]
-                :=  || [ or ]
-                :=  != [ diferente ]
-                :=  +  [ sumar ]
-                :=  -  [ restar ]
-                :=  *  [ multiplicar ]
-                :=  /  [ dividir ]
-                :=  mod [ módulo ]
-
-         strings
-                :=  concat [ concatenar ]
-
-          listas
-                :=  join [ join-lista ]
-                :=  push [ push-lista ]
-
 |#
 
 ;; LEXICAS
@@ -253,7 +119,7 @@
 
 ;*******************************************************************************************
 ;; PROMPT
-(define Mixer.exe
+(define mixer.exe
   (sllgen:make-rep-loop  "@Mixer -> "
     (lambda (programa) (eval-program programa)) 
     (sllgen:make-stream-parser lexica gramatical)))
@@ -263,99 +129,16 @@
 (define eval-program
   (lambda (pgm)
    (cases programa pgm
-     (un-programa (exp) exp))))
+     (un-programa (exp) (eval-expresion exp)))))
 
 ;*******************************************************************************************
-;; TEST DEFITIONS TO SYNTAX TREE
-
-(parser "1")
-(parser "1.1")
-(parser "-1")
-(parser "-1.1")
-(parser "//comentario
-          'message'
-         //comentario")
-
-(parser "true")
-(parser "false")
-
-(parser "'mensaje'")
-(parser "$var")
-
-(parser "if ((1+2)>4) { true } else { false }")
-
-(parser "x32 [1 0 2 3]")
-(parser "x16 [1 0 2 3]")
-(parser "x8  [1 0 2 3]")
-
-(parser "!(false)")
-(parser "++($var)")
-(parser "--($var)")
-(parser "strlen($var)")
-(parser "isNull($var)")
-(parser "isList($var)")
-(parser "pop($var)")
-(parser "next($var)")
-
-(parser "(1 + 2)")
-(parser "(1 - 2)")
-(parser "(5 * 2)")
-(parser "(1 / 2)")
-(parser "(1 mod 2)")
-
-(parser "(5 > 2)")
-(parser "(1 < 2)")
-(parser "(1 >= 2)")
-(parser "(1 <= 2)")
-(parser "(1 == 2)")
-
-(parser "(true != false)")
-(parser "(true || false)")
-(parser "(true && true)")
-
-(parser "('hola' concat 'adios')")
-(parser "([1] join [2,6])")
-(parser "([1,2] push [3,4])")
-(parser "++32 (x32[1 2 3])")
-(parser "--16 (x16[1 2 3])")
-(parser "(x32[1 2 3] +32 x32[1 2 3])")
-(parser "(x32[1 2 3] *32 x32[1 2 3])")
-
-
-
-;; ++ COMPLEX 
-
-(parser "export func $factorial ($x) => {
-           if ($x == 0){1} else {( $x * import $factorial (--($x)) )}}")
-
-(parser "export func $fibonacci ($x) => {
-           if (($x == 0) || ($x == 1)){1} else {
-                              ( import $fibonacci (--($x)) + import $fibonacci (($x - 2)) )}}")
-
-(parser "private def (static $s, init static $x = 3, const $y = 5){(($s+1) * $y)}")
-
-(parser "private def (static $XXX, init static $YYYY = 3, const $ZZZZZ = 5)
-           { private def (static $AAA, init static $BBBB = 3, const $CCCC = 5){(($s+1) * $y)}}")
-
-(parser "private def (init static $x = func ($x) => {++($x)})
-{import $x (5)}")
-
-(parser "import func ($y) => {++($y)} (5)")
-
-
-;; VALIDACION 2
-;; BASIC
-(parser "import export func $factorial ($x) => {
-           if ($x == 0){1} else {( $x * import $factorial (--($x)) )}} (3)")
-
-;;ADVANCED
-(parser "private def (init static $VarX = 3,
-                      const $Fact = export func $factorial ($x) => {
-                          if ($x == 0){ 1 } else { ( $x * import $factorial (--($x)) )}})
-                     {import $Fact ($VarX)}")
-
-
-
+;; EVAL-EXPORESION
+(define eval-expresion
+  (lambda (exp)
+   (cases expresion exp
+     (numero-exp (num) num)
+     (else 'falta-implementar)
+     )))
 
 
 
